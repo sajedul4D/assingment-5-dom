d2d0315133e2832ebb3544b0b2b29fde018459d1
sajedul4D <sajedultasin17@gmail.com>
2024-12-19 18:09:22 +0600

    hidden and show funciton

--- js/index.js
+++ js/index.js
@@ -22,6 +22,12 @@ else {
     document.getElementById('total-amount').innerText = UpdatedBalanceNumber.toFixed(2);
 
 }
+const div=document.createElement('div');
+div.classList=' font-bold space-y-4 p-6 border-2 border-gray-300 border-dotted'
+div.innerHTML=`<p class=text-2xl font-bold >${donate} Taka is Donated for famine-2024 at Noakhali, Bangladesh</p>
+<p>${new Date()}</p>`
+const headContainer=document.getElementById('history-container');
+headContainer.insertBefore(div,headContainer.firstChild)
 
 document.getElementById("my_modal_1").showModal();
 
@@ -44,6 +50,13 @@ document.getElementById('donate-2').addEventListener('click',function(){
        const MyAmount=total_amount-donate;
       document.getElementById('total-amount').innerText=MyAmount
     }
+    const div=document.createElement('div');
+    div.classList=' font-bold space-y-4 p-6 border-2 border-gray-300 border-dotted'
+    div.innerHTML=`<p class=text-2xl font-bold >${donate} Taka is Donated for famine-2024 at feni, Bangladesh</p>
+    <p>${new Date()}</p>`
+    const headContainer=document.getElementById('history-container');
+    headContainer.insertBefore(div,headContainer.firstChild)
+
     document.getElementById("my_modal_1").showModal();
 })
 
@@ -66,6 +79,13 @@ document.getElementById('donate-3').addEventListener('click',function(){
     const QuotaBalance=total_amount-donatAmount;
     document.getElementById('total-amount').innerText=QuotaBalance
    }
+
+   const div=document.createElement('div');
+div.classList=' font-bold space-y-4 p-6 border-2 border-gray-300 border-dotted'
+div.innerHTML=`<p class=text-2xl font-bold >${donatAmount} Taka is Donated for Injured in the Quota, Bangladesh</p>
+<p>${new Date()}</p>`
+const headContainer=document.getElementById('history-container');
+headContainer.insertBefore(div,headContainer.firstChild)
    document.getElementById('my_modal_1').showModal();
 })
 // kids 
@@ -87,5 +107,31 @@ document.getElementById('donate-4').addEventListener('click',function(){
     const kidsBalance=total_amount-donatAmount;
     document.getElementById('total-amount').innerText=kidsBalance
    }
+   const div=document.createElement('div');
+   div.classList=' font-bold space-y-4 p-6 border-2 border-gray-300 border-dotted'
+   div.innerHTML=`<p class=text-2xl font-bold >${donatAmount} Taka is Donated for  Palestinians kids gaza,  Palestinians</p>
+   <p>${new Date()}</p>`
+   const headContainer=document.getElementById('history-container');
+   headContainer.insertBefore(div,headContainer.firstChild)
+
    document.getElementById('my_modal_1').showModal();
+})
+
+
+
+const Donation=document.getElementById('Donation');
+const history=document.getElementById('History');
+history.addEventListener('click',function(){
+    history.classList.add('bg-[#B4F461]')
+    Donation.classList.remove('bg-[#B4F461]');
+    document.getElementById('history-container').classList.remove('hidden');
+    document.getElementById('main-container').classList.add('hidden')
+})
+
+Donation.addEventListener('click',function(){
+  Donation.classList.add('bg-[#B4F461]')
+  history.classList.remove('bg-[#B4F461]')
+  document.getElementById('main-container').classList.remove('hidden');
+  document.getElementById('history-container').classList.add('hidden')
+
 })
